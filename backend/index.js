c23550c4d8997021dfb004be1d23468ca5dc3975
maribelcuales <maribelcuales@gmail.com>
2021-11-02 21:10:55 +0800

    Created and added env variables.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
+require('dotenv').config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE, 
+  }
+});
 
 const app = express(); 
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); 
+
+// Cors prevents errors when we try to access the server from a different server location
 app.use(cors());
 
 app.get('/', (req, res) => res.send("Home Route"));
