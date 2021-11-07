33f0aa49e880af27f6d92462801efc862ba1673a
maribelcuales <maribelcuales@gmail.com>
2021-11-07 16:31:25 +0800

    Created landing page.

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,29 +1,67 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import styled from "styled-components"; 
+import "./styles.css";
+import Popcorn from "./popcorn.png";
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column; 
-  box-sizing: border-box; 
+  flex-direction: column;
+  box-sizing: border-box;
+  height: 100%;
 `;
 
-const BackgroundImage = styled.img`
-  width: 100%;
-  height: 100%; 
-  background-image: url("/media/popcorn.png");
-  background-position: center; 
+const Background = styled.img`    
+  height: auto;
+  width: 100%
+  background-attachment: fixed;
+  background-position: center;
   background-repeat: no-repeat;
-  background-size: cover;   
+  background-size: cover;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  position: absolute;
+  top: 40%;
+  left: 35%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 70px;
+`;
+
+const Button = styled.button`
+  padding: 1rem;
+  border: none;
+  border-radius: 15px;
+  background: white;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  font-weight: bold;
+  color: #013a63;
+  font-size: 20px;
+  cursor: pointer;
+  transition: background 0.3s;
+  text-transform: uppercase;
+
+  top: 52%;
+  left: 30%;
+  text-align: center;
+  position: absolute;
+  width: 200px;
+  background-color: white;
 `;
 
 const Landing = () => {
   return (
     <Container>
-      <h1>Meta Movie Reviews</h1> 
-      <BackgroundImage src="/media/popcorn.png" alt="Popcorn" class="landing-page-image"/> 
+      <Background src={Popcorn} alkt="Popcorn Background"/> 
+        <div>
+          <Title>Meta Movie Reviews</Title>
+          <Button>Add Review</Button> 
+        </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Landing; 
+export default Landing;
