3267931dba2ad0501429ef35d70d927fc30d0123
maribelcuales <maribelcuales@gmail.com>
2021-11-08 21:50:22 +0800

    Created movies component.

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -26,7 +26,7 @@ const Movies = () => {
   return (
     <Fragment>
       <header>
-        <h1>Meta Movie Reviews</h1>
+        <h1>Movies</h1>
       </header>
       <main>
         {loading === true ? (
