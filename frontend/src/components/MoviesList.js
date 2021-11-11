8636f717ee8c6fccc1236d72d8e2547b7726de83
maribelcuales <maribelcuales@gmail.com>
2021-11-11 23:13:06 +0800

    Initial styling added.

--- frontend/src/components/MoviesList.js
+++ frontend/src/components/MoviesList.js
@@ -1,39 +1,46 @@
-import React from 'react'; 
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import '../App.css';
-import Cinema from './cinemaFrontage.jpg'; 
+import "../App.css";
+import Cinema from "./cinemaFrontage.jpg";
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-evenly; 
-  height: 100%;  
+  justify-content: space-evenly;
+  height: 100%;
 `;
 
 const Background = styled.img`
-  max-width: 100%; 
-  background-position: center; 
+  max-width: 100%;
+  background-position: center;
   background-repeat: repeat;
-  background-size: cover; 
-  background-attachment: fixed;  
+  background-size: cover;
+  background-attachment: fixed;
 `;
 
 const SectionDiv = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly; 
-  margin: 4rem; 
+  justify-content: space-evenly;
+  margin: 4rem;
 `;
 
 const MoviesList = () => {
-  return(
+  return (
     <Container>
-      <h1>Curated Meta Movie Reviews</h1>
-      <div>
-        <Background src={Cinema} alt="Cinema Frontage" />
-      </div>
+      <SectionDiv>
+        <div
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        >
+          <h1>Curated Meta Movie Reviews</h1>
+          <Background src={Cinema} alt="Cinema Frontage" />
+        </div>
+      </SectionDiv>
     </Container>
   );
 };
 
-export default MoviesList; 
+export default MoviesList;
