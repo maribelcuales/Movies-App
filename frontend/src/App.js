ea25b58493221414e1635832ed4405104041a75c
maribelcuales <maribelcuales@gmail.com>
2021-11-11 23:07:19 +0800

    Added MoviesList component to its movies-reviews route.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Landing from './components/Landing.js';
 import AddMovie from "./components/AddMovie.js";
 import Movies from "./components/Movies.js";
+import MoviesList from "./components/MoviesList.js";
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Landing}/>
         <Route exact path="/movies" component={Movies}/>
-        <Route exact path="/movies-reviews" component={Movies}/>
+        <Route exact path="/movies-reviews" component={MoviesList}/>
         <Route exact path="/add-movie" component={AddMovie}/>
       </Switch>
     </div>
