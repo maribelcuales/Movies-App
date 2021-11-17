7ad1f5377e541d90cbdc89697b7aff27c966fcde
maribelcuales <maribelcuales@gmail.com>
2021-11-17 21:41:58 +0800

    Created MetaMovies component and added its route to App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Landing from './components/Landing.js';
 import AddMovie from "./components/AddMovie.js";
 import Movies from "./components/Movies.js";
 import MoviesList from "./components/MoviesList.js";
+import MetaMovies from "./components/MetaMovies.js";
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Landing}/>
+        <Route exact path="/home" component={MetaMovies}/>
         <Route exact path="/movies" component={Movies}/>
         <Route exact path="/movies-reviews" component={MoviesList}/>
         <Route exact path="/add-movie" component={AddMovie}/>
