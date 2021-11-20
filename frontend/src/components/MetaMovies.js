0ba55c75355f90c8eab422e9cce7fbb1bf1c774f
maribelcuales <maribelcuales@gmail.com>
2021-11-20 15:58:41 +0800

    Styling added to nav component.

--- frontend/src/components/MetaMovies.js
+++ frontend/src/components/MetaMovies.js
@@ -1,19 +1,24 @@
+import "./MetaHeader.js";
+import MetaHeader from "./MetaHeader.js";
 import "./metaStyles.css";
 
 const MetaMovies = () => {
   return (
-    <div className="banner" id="banner">
-      <div className="content">
-        <h2>Meta Movie Reviews</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <a href="#" className="btn">
-          Movie List
-        </a>
+    <div>
+      <MetaHeader /> 
+      <div className="banner" id="banner">
+        <div className="content">
+          <h2>Meta Movie Reviews</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <a href="#" className="btn">
+            Movie List
+          </a>
+        </div>
       </div>
     </div>
   );
